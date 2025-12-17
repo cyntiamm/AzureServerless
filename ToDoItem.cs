b18d07e547311b198a155009bc356cc68ac4f8c2
cyntiamm <99305079+cyntiamm@users.noreply.github.com>
2025-12-16 22:14:40 -0300

    ToDoItem
    Program to imput new data in database.

---
+++ ToDoItem.cs
@@ -0,0 +1,11 @@
+{
+	public class ToDoItem
+	{
+		public Guide Id { get; set; }
+		public int? order { get; set; }
+		public string title { get; set; }
+		public string url { get; set; }
+		public bool? completed { get; set; }
+	}
+
+}
